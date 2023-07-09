ec007a974319de5cce0f277aaa2ca6497b0071b2
Jinish Patel <jinish5597@gmail.com>
2023-07-09 11:13:23 -0400

    Add project files.

---
+++ Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            ViewBag.BmiValue = 0;
+
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Index(Jinish_Assignement1 model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.BMI_236 = model.CalculateBMI();
+            }
+            else
+            {
+                ViewBag.BmiValue = 0;
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Assignment1/Models/Jinish_Assignement1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace Assignment1.Models;
+
+
+
+public class Jinish_Assignement1
+{
+
+
+
+    [Required(ErrorMessage = "Please first enter Weight of the body.")]
+    public decimal? Weight_236 { get; set; }
+
+    [Required(ErrorMessage = "Please first enter height of the body.")]
+    public decimal? Height_236 { get; set; }
+    public decimal? CalculateBMI()
+    {
+        decimal? BMI_236 = 0;
+
+        BMI_236 = (Weight_236 / (Height_236 * Height_236)) * 703;
+        return Math.Round((decimal)BMI_236, 2); 
+    }
+}
